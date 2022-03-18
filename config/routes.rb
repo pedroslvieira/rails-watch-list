547db1c5ee5b35cdee91df8d7b094be31f224a44
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-03-18 12:23:35 +0100

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, except: :index do
+  resources :lists do
     resources :bookmarks, only: [ :new, :create, :destroy ]
   end
 end
